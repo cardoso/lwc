15107c7649a4832f30a1307b91503d4d9e5c76db
Matheus Cardoso <matheus@cardo.so>
2024-11-20 10:34:00 -0300

    feat: use import.meta.glob

--- packages/@lwc/engine-server/src/__tests__/fixtures.spec.ts
+++ packages/@lwc/engine-server/src/__tests__/fixtures.spec.ts
@@ -4,11 +4,11 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+/// <reference types="vite/client" />
 import path from 'node:path';
 import { vi, test, beforeAll, describe } from 'vitest';
 import { rollup } from 'rollup';
 import lwcRollupPlugin, { type RollupLwcOptions } from '@lwc/rollup-plugin';
-import { glob } from 'glob';
 import { formatHTML } from '@lwc/test-utils-lwc-internals';
 import * as lwc from '../index';
 
@@ -102,10 +102,14 @@ async function compileFixtures(
 }
 
 const fixtureDir = path.resolve(__dirname, `./fixtures`);
-const fixtures = glob.sync('**/index.js', {
-    ignore: ['**/dist/**'],
-    cwd: fixtureDir,
-});
+const fixtures = Object.keys(
+    // @ts-expect-error import.meta
+    import.meta.glob<string>('./fixtures/**/index.js', {
+        query: '?url',
+        eager: true,
+        import: 'default',
+    })
+).map((k) => k.replace('./fixtures/', ''));
 
 const input = Object.fromEntries(fixtures.map((f) => [f, path.resolve(fixtureDir, f)]));
 
